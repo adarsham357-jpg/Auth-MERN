f7f4e3311afc03bd2f9e6a11ac1bc8f63ade4d91
adarsham357-jpg <your_adarsha_github_email@gmail.com>
2026-05-02 11:39:50 +0530

    updated .env

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
+
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('dotenv').config();
 
-
-
-
-
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-connectDb();
+connectDB();
 
 app.use('/api/users', require('./routes/UserRoutes'));
 
-app.listen(3000, () => {
-    console.log('server is running on http://localhost:3000');
+app.listen(5000, () => {
+    console.log('server is running on http://localhost:5000');
 });
